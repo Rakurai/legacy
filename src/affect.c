c8f52cdd3d7a473e12faf0421061a155d7a243f0
Jason Anderson <jwa2@clemson.edu>
2016-11-02 09:46:56 -0400

    Reworked affect_parse_prototype a little for better checking, and testing attr locations.

--- src/affect.c
+++ src/affect.c
@@ -140,17 +140,65 @@ int affect_bit_to_sn(int bit) {
 	return -1;
 }
 
+int affect_attr_location_check(int location) {
+	switch (location) {
+		case APPLY_NONE          : return APPLY_NONE;
+		case APPLY_STR           : return APPLY_STR;
+		case APPLY_DEX           : return APPLY_DEX;
+		case APPLY_INT           : return APPLY_INT;
+		case APPLY_WIS           : return APPLY_WIS;
+		case APPLY_CON           : return APPLY_CON;
+		case APPLY_SEX           : return APPLY_SEX;
+		case APPLY_AGE           : return APPLY_AGE;
+		case APPLY_MANA          : return APPLY_MANA;
+		case APPLY_HIT           : return APPLY_HIT;
+		case APPLY_STAM          : return APPLY_STAM;
+		case APPLY_GOLD          : return APPLY_GOLD;
+		case APPLY_EXP           : return APPLY_EXP;
+		case APPLY_AC            : return APPLY_AC;
+		case APPLY_HITROLL       : return APPLY_HITROLL;
+		case APPLY_DAMROLL       : return APPLY_DAMROLL;
+		case APPLY_SAVING_PARA   :
+		case APPLY_SAVING_ROD    :
+		case APPLY_SAVING_PETRI  :
+		case APPLY_SAVING_BREATH : return APPLY_SAVES;
+		case APPLY_SAVING_SPELL  : return APPLY_SAVING_SPELL;
+		case APPLY_SPELL_AFFECT  : return APPLY_SPELL_AFFECT;
+		case APPLY_CHR           : return APPLY_CHR;
+		case APPLY_SHEEN         : return APPLY_SHEEN;
+		case APPLY_BARRIER       : return APPLY_BARRIER;
+		case APPLY_FOCUS         : return APPLY_FOCUS;
+	}
+
+	bugf("affect_attr_location_check: bad location %d", location);
+	return -1;
+}
+
 // perform the error checking in building affects from a prototype, including bits.
 // fills an AFFECT_DATA struct with one of the bits, removes the bit from
 // the vector.  return value indicates whether the struct is valid to insert.
 // assumes type, level, duration, evolution, location and modifier already filled,
 // but alters if appropriate
 bool affect_parse_prototype(char letter, AFFECT_DATA *paf, unsigned int *bitvector) {
+	switch (letter) {
+	case 'O': paf->where = TO_OBJECT; break; // location and modifier already set
+	case 'A': paf->where = TO_AFFECTS; break; // location and modifier already set
+	case 'D': paf->where = TO_DEFENSE; break; // modifier already set
+	case 'I': paf->where = TO_DEFENSE; paf->modifier = 100; break;
+	case 'R': paf->where = TO_DEFENSE; paf->modifier = 50; break;
+	case 'V': paf->where = TO_DEFENSE; paf->modifier = -50; break;
+	default:
+		bugf("affect_parse_prototype: bad letter %c", letter);
+		return FALSE;
+	}
+
 	// weird case, defense flag A used to be *_SUMMON, changed to a ACT_NOSUMMON.
 	// wouldn't be a concern except the index is repurposed to a cache counter.
 	if (paf->where == TO_DEFENSE)
 		REMOVE_BIT(*bitvector, A);
 
+	// treat the bitvector as an array, find the lowest index with a set bit, remove it from
+	// the vector, and use the index to match against new constants
 	unsigned int bit = 1;
 	int index = 0;
 
@@ -165,23 +213,13 @@ bool affect_parse_prototype(char letter, AFFECT_DATA *paf, unsigned int *bitvect
 	// if the bit wasn't found, still continue for the TO_OBJECT.  the loop will
 	// stop when bitvector is 0
 
-	switch (letter) {
-	case 'O': paf->where = TO_OBJECT; break; // location and modifier already set
-	case 'A': paf->where = TO_AFFECTS; break; // location and modifier already set
-	case 'D': paf->where = TO_DEFENSE; break; // modifier already set
-	case 'I': paf->where = TO_DEFENSE; paf->modifier = 100; break;
-	case 'R': paf->where = TO_DEFENSE; paf->modifier = 50; break;
-	case 'V': paf->where = TO_DEFENSE; paf->modifier = -50; break;
-	default:
-		bugf("affect_parse_prototype: bad letter %c", letter);
-		return FALSE;
-	}
-
 	if (paf->where == TO_DEFENSE) {
-		if (index < 1 || index > 32) // no bits, not an error, just skip it
+		if (index < 1 || index >= 32) // no bits, not an error, just skip it
 			return FALSE;
 
 		paf->location = index;
+		// modifier was already set or done above
+		paf->bitvector = 0;
 		return TRUE;
 	}
 
@@ -193,15 +231,21 @@ bool affect_parse_prototype(char letter, AFFECT_DATA *paf, unsigned int *bitvect
 		}
 
 		paf->type = sn;
-		// drop down to applies
+		paf->bitvector = 0;
+		// drop down to applies for possible location and modifier
 	}
+
+	// 'O' apply could theoretically be used to add extra bits to an object,
+	// but for now we'll just zero it to make sure weird stuff doesn't happen
+	paf->bitvector = 0;
+
+	paf->location = affect_attr_location_check(paf->location);
+
+	if (paf->location == -1)
+		return FALSE;
 
 	if (paf->location == 0)
 		paf->modifier = 0; // ensure, so we don't mess up the counter
-	else if (paf->location < 1 || paf->location > 32) {
-		bugf("affect_parse_prototype: bad location %d", paf->location);
-		return FALSE;
-	}
 
 	return TRUE;
 }
