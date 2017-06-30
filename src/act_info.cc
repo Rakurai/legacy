cb0b9608d1d69261010210ae12d541b4051d7ebf
Jason Anderson <jwa2@clemson.edu>
2017-06-30 16:57:14 -0400

    Fixed immname alignment bug in wholist for imms.

--- src/act_info.cc
+++ src/act_info.cc
@@ -2567,8 +2567,22 @@ void do_who(Character *ch, String argument)
 		if (IS_IMMORTAL(wch) && !wch->pcdata->immname.empty()) {
 			/* copy immname without the brackets */
 			block1 = wch->pcdata->immname;
-			block1 = block1.substr(block1.find("[")+1);
-			block1 = block1.substr(0, block1.find("]")-1);
+
+			for (int i = 0; i < block1.size(); ++i) {
+				char c = block1[i];
+				block1[i] = ' ';
+				if (c == '[')
+					break;
+			}
+
+			for (int i = block1.size()-1; i >= 0; --i) {
+				char c = block1[i];
+				block1[i] = ' ';
+				if (c == ']')
+					break;
+			}
+
+			block1 = block1.strip().center(15);
 		}
 		else {
 			if (char_at_war(wch)) {
