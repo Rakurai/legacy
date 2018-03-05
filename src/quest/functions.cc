e782c92e318d20ee6823bf6dafdf5f95f9fa6119
Jason Anderson <jwa2@clemson.edu>
2018-03-05 08:20:20 -0500

    Quest reward rolling done.  Rolling object rewards works.
    Changed quest 'xp' reward to 'exp'.

--- src/quest/functions.cc
+++ src/quest/functions.cc
@@ -5,8 +5,10 @@
 #include "Game.hh"
 #include "Logging.hh"
 #include "Character.hh"
+#include "Object.hh"
 #include "quest/functions.hh"
 #include "World.hh"
+#include "random.hh"
 
 namespace quest {
 
@@ -63,11 +65,87 @@ void assign(Player *player, const Quest *quest) {
 
 // only called from quest::progress, error checking done
 void complete(Player *player, const Quest *quest) {
+	Character *ch = player->ch;
+
+	ptc(ch, "{Y%s - QUEST COMPLETE!{x\n", quest->name);
+
 	// roll some loot!
-	for (const auto& reward : quest->rewards) {
-		Logging::bugf("rolling reward");
+	for (const auto& params : quest->rewards) {
+		if (!roll_chance(params.pct_chance))
+			continue;
+
+		int amount = number_range(params.amount_min, params.amount_max);
+
+		if (params.type == "gold") {
+			ptc(ch, "You receive %d gold coin%s.\n",
+				amount, amount > 1 ? "s" : "");
+			ch->gold += amount;
+		}
+		else if (params.type == "silver") {
+			ptc(ch, "You receive %d silver coin%s.\n",
+				amount, amount > 1 ? "s" : "");
+			ch->silver += amount;
+		}
+		else if (params.type == "exp") {
+			if (!ch->revoke_flags.has(REVOKE_EXP)) {
+				ptc(player->ch, "You receive %d experience point%s.\n",
+					amount, amount > 1 ? "s" : "");
+				gain_exp(ch, amount);
+			}
+		}
+		else if (params.type == "qp") {
+			ptc(player->ch, "You receive %d quest point%s.\n",
+				amount, amount > 1 ? "s" : "");
+			player->questpoints += amount;
+		}
+		else if (params.type == "sp") {
+			ptc(player->ch, "You receive %d skill point%s.\n",
+				amount, amount > 1 ? "s" : "");
+			player->skillpoints += amount;
+		}
+		else if (params.type == "prac") {
+			ptc(player->ch, "You receive %d practice%s.\n",
+				amount, amount > 1 ? "s" : "");
+			ch->practice += amount;
+		}
+		else if (params.type == "train") {
+			ptc(player->ch, "You receive %d train%s.\n",
+				amount, amount > 1 ? "s" : "");
+			ch->train += amount;
+		}
+		else if (params.type == "obj") {
+			Vnum vnum(params.what);
+			ObjectPrototype *proto = Game::world().get_obj_prototype(vnum);
+
+			if (proto == nullptr)
+				Logging::bugf("quest::complete: unable to find object with vnum %d", vnum);
+			else {
+				for (int i = 0; i < amount; i++) {
+					Object *obj = create_object(proto, 0);
+
+					if (obj == nullptr) {
+						Logging::bugf("quest::complete: unable to create object with vnum %d", vnum);
+						break;
+					}
+
+					if (CAN_WEAR(obj, ITEM_TAKE)) {
+						ptc(player->ch, "You receive %s!\n", obj->short_descr);
+						obj_to_char(obj, ch);
+					}
+					else {
+						ptc(player->ch, "%s appears before you!\n", obj->short_descr);
+						obj_to_room(obj, ch->in_room);
+					}
+				}
+			}
+		}
+
+		// quit after successful roll if stop is set
+		if (params.stop)
+			break;
 	}
 
+	ptc(ch, "{x");
 	// remove the quest
 	remove(player, quest);
 	player->completed_quests.insert(quest->id);
