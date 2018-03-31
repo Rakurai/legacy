28b29290332a77fefd1603e23bd228a33870a0d1
Jason Anderson <jwa2@clemson.edu>
2018-03-31 17:59:39 -0400

    fixed comparison of rooms to use location, not name

--- src/progs/symbols/symbols.cc
+++ src/progs/symbols/symbols.cc
@@ -11,7 +11,7 @@ namespace symbols {
 const String var_to_string(World * var) { return var ? "World" : "0"; }
 const String var_to_string(Character * var) { return var ? var->name : "0"; }
 const String var_to_string(Object * var) { return var ? var->name : "0"; }
-const String var_to_string(Room * var) { return var ? var->name() : "0"; }
+const String var_to_string(Room * var) { return var ? var->location.to_string() : "0"; }
 const String var_to_string(bool var) { return var ? "1" : "0"; }
 const String var_to_string(int var) { return Format::format("%d", var); }
 const String var_to_string(const String& var) { return var; }
